0171e12b69bdca0008cead9a5ab0b62cba1f4188
Tunji Paul <ogorpaul877@gmail.com>
2025-11-10 22:34:25 +0100

    updated

--- Button-Component-Library/src/App.jsx
+++ Button-Component-Library/src/App.jsx
@@ -3,6 +3,7 @@ import ButtonComponent from "./ButtonComponent";
 function App() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="absolute top-0 left-0 text-green-500 font-bold">Button Component Library</h1>
       <div className="bg-white p-6 rounded-xl shadow flex flex-col items-center space-y-4 w-128">
         <ButtonComponent type="primary">Primary</ButtonComponent>
         <ButtonComponent type="secondary">Secondary</ButtonComponent>
